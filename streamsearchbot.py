f33350347bb07a17cc409150ce1637a763473a91
Kidiloska <73132025+kidiloskahyper45@users.noreply.github.com>
2020-12-28 13:47:27 +0530

    Update streamsearchbot.py

--- streamsearchbot.py
+++ streamsearchbot.py
@@ -24,7 +24,7 @@ torrentbot = bot.start(bot_token=Config.BOT_TOKEN)
 async def search(event):
     replied_user = await event.client(GetFullUserRequest(event.sender_id))
     firstname = replied_user.user.first_name
-    await event.reply(message=f"**Hello🚴, {firstname}, I Am Inline Stream Search 🤖Bot.** \n**Using Me You Can Fetch Torrent Magnet, 🚀Youtube Videos Link, Jio Saavan Music Links** \n**(C) @TG_BotZ**",
+    await event.reply(message=f"**Hello🚴, {firstname}, I Am Inline Stream Search 🤖Bot.** \n**Using Me You Can Fetch 🏓Torrent Magnet, 🚀Youtube Videos Link, ⚡Jio Saavan Music Links** \n**(C) @TG_BotZ**",
                       buttons=[
                       [Button.switch_inline("Search Youtube", query="yt ", same_peer=True)],
                       [Button.switch_inline("Search Torrent", query="torrent ", same_peer=True)],
@@ -173,7 +173,7 @@ async def inline_handler(event):
         if query == None or " ": 
             resulte = builder.article(
                 title="Usage Guide.",
-                description="(C) @StarkGanG",
+                description="(C) @TG_BotZ",
                 text=f"**How To Use Me?** \n**Youtube :** `@{firstname} yt <query>` \n**Example :** `@{firstname} yt why we lose song` \n\n**Torrent :** `@{firstname} torrent <query>` \n**Example :** `@{firstname} torrent avengers endgame ` \n\n**JioSaavan :** `@{firstname} jm <query>` \n**Example :** `@{firstname} jm dilbaar`",
                 buttons=[
                       [Button.url("Contact Me", f"t.me/{firstname}")],
